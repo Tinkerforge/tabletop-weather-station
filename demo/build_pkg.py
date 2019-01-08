451a49318dedaaf38f6ec7f81a5cafa45ab37d55
Matthias Bolte <matthias@tinkerforge.com>
2019-01-08 15:40:30 +0100

    demo: Add installer

---
+++ demo/build_pkg.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Tabletop Weather Station Demo
+Copyright (C) 2019 Matthias Bolte <matthias@tinkerforge.com>
+
+build_pkg.py: Package builder for Tabletop Weather Station Demo
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+# Windows:
+#   dependencies:
+#       pythonxy (2.6)
+#       py2exe
+#       nsis
+#       win redistributables vcredist under winxp
+
+import os
+import sys
+import base64
+import shutil
+import struct
+import subprocess
+from tabletop_weather_station_demo.config import DEMO_VERSION
+
+
+UNDERSCORE_NAME = 'tabletop_weather_station_demo'
+CAMEL_CASE_NAME = 'Tabletop Weather Station Demo'
+
+
+def system(command):
+    if os.system(command) != 0:
+        sys.exit(1)
+
+
+def check_output(*args, **kwargs):
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden')
+
+    process = subprocess.Popen(stdout=subprocess.PIPE, *args, **kwargs)
+    output, error = process.communicate()
+    exit_code = process.poll()
+
+    if exit_code != 0:
+        command = kwargs.get('args')
+
+        if command == None:
+            command = args[0]
+
+        raise subprocess.CalledProcessError(exit_code, command, output=output)
+
+    return output
+
+
+def specialize_template(template_filename, destination_filename, replacements):
+    template_file = open(template_filename, 'rb')
+    lines = []
+    replaced = set()
+
+    for line in template_file.readlines():
+        for key in replacements:
+            replaced_line = line.replace(key, replacements[key])
+
+            if replaced_line != line:
+                replaced.add(key)
+
+            line = replaced_line
+
+        lines.append(line)
+
+    template_file.close()
+
+    if replaced != set(replacements.keys()):
+        raise Exception('Not all replacements for {0} have been applied'.format(template_filename))
+
+    destination_file = open(destination_filename, 'wb')
+    destination_file.writelines(lines)
+    destination_file.close()
+
+
+def freeze_images():
+    directory = UNDERSCORE_NAME
+    image_files = []
+
+    for root, dirnames, names in os.walk(directory):
+        for name in names:
+            full_name = os.path.join(root, name)
+
+            if os.path.isfile(full_name):
+                _, ext = os.path.splitext(name)
+                ext = ext[1:]
+
+                if ext in ['bmp', 'png', 'jpg']:
+                    image_files.append([full_name.replace('\\', '/').replace(directory + '/', ''), ext])
+
+    images = open(os.path.join(directory, 'frozen_images.py'), 'wb')
+    images.write('image_data = {\n'.encode('utf-8'))
+
+    for image_file in image_files:
+        image_data = base64.b64encode(file(os.path.join(directory, image_file[0]), 'rb').read())
+        images.write("'{0}': ['{1}', '{2}'],\n".format(image_file[0], image_file[1], image_data).encode('utf-8'))
+
+    images.write('}\n'.encode('utf-8'))
+    images.close()
+
+
+def build_macos_pkg():
+    print('building demo disk image')
+    root_path = os.getcwd()
+
+    print('removing old build directories')
+    build_path = os.path.join(root_path, 'build')
+    dist_path = os.path.join(root_path, 'dist')
+
+    if os.path.exists(build_path):
+        shutil.rmtree(build_path)
+
+    if os.path.exists(dist_path):
+        shutil.rmtree(dist_path)
+
+    print('freezing images')
+    freeze_images()
+
+    print('calling setup.py py2app build')
+    system('python setup.py py2app build')
+
+    print('copying build data')
+    build_data_path = os.path.join(root_path, 'build_data', 'macos', '*')
+    resources_path = os.path.join(dist_path, '{0}.app'.format(CAMEL_CASE_NAME), 'Contents', 'Resources')
+    system('cp -R {0} "{1}"'.format(build_data_path, resources_path))
+
+    print('patching __boot__.py')
+    boot_path = os.path.join(resources_path, '__boot__.py')
+    boot_prefix = 'import os\nimport sys\nos.environ["RESOURCEPATH"] = os.path.dirname(os.path.realpath(__file__))\n'
+
+    with open(boot_path, 'rb') as f:
+        boot = f.read()
+
+    with open(boot_path, 'wb') as f:
+        f.write(boot_prefix + boot)
+
+    print('building disk image')
+    dmg_name = '{0}_macos_{1}.dmg'.format(UNDERSCORE_NAME, DEMO_VERSION.replace('.', '_'))
+
+    if os.path.exists(dmg_name):
+        os.remove(dmg_name)
+
+    system('hdiutil create -fs HFS+ -volname "{0}-{1}" -srcfolder dist {2}'
+           .format(CAMEL_CASE_NAME.replace(' ', '-'), DEMO_VERSION, dmg_name))
+
+
+# https://github.com/rfk/www.rfk.id.au/blob/master/content/blog/entry/code-signing-py2exe/index.html
+def sign_py2exe(exepath):
+    # First, sign a *copy* of the file so that we know its final size.
+    execopy = os.path.join(os.path.dirname(exepath), 'temp-' + os.path.basename(exepath))
+    shutil.copy2(exepath, execopy)
+    system('X:\\sign.bat ' + execopy)
+
+    # Figure out the size of the appended signature.
+    comment_size = os.stat(execopy).st_size - os.stat(exepath).st_size
+    os.unlink(execopy)
+
+    # Write the correct comment size as the last two bytes of the file.
+    with open(exepath, "r+") as f:
+        f.seek(-2, os.SEEK_END)
+        f.write(struct.pack("<H", comment_size))
+
+    # Now we can sign the file for real.
+    system('X:\\sign.bat ' + exepath)
+
+
+def build_windows_pkg():
+    print('building demo NSIS installer')
+    root_path = os.getcwd()
+
+    print('removing old build directories')
+    build_path = os.path.join(root_path, 'build')
+    dist_path = os.path.join(root_path, 'dist')
+
+    if os.path.exists(build_path):
+        shutil.rmtree(build_path)
+
+    if os.path.exists(dist_path):
+        shutil.rmtree(dist_path)
+
+    print('freezing images')
+    freeze_images()
+
+    print('calling setup.py py2exe')
+    system('python setup.py py2exe')
+
+    # FIXME: doesn't work yet
+    #if os.path.exists('X:\\sign.bat'):
+    #    sign_py2exe('dist\\{0}.exe'.format(UNDERSCORE_NAME))
+
+    print('creating NSIS script from template')
+    nsis_template_path = os.path.join(root_path, 'build_data', 'windows', 'nsis', '{0}_installer.nsi.template'.format(UNDERSCORE_NAME))
+    nsis_path = os.path.join(dist_path, 'nsis', '{0}_installer.nsi'.format(UNDERSCORE_NAME))
+    specialize_template(nsis_template_path, nsis_path,
+                        {'<<DEMO_DOT_VERSION>>': DEMO_VERSION,
+                         '<<DEMO_UNDERSCORE_VERSION>>': DEMO_VERSION.replace('.', '_')})
+
+    print('building NSIS installer')
+    system('"C:\\Program Files (x86)\\NSIS\\makensis.exe" dist\\nsis\\{0}_installer.nsi'.format(UNDERSCORE_NAME))
+    installer = '{0}_windows_{1}.exe'.format(UNDERSCORE_NAME, DEMO_VERSION.replace('.', '_'))
+
+    if os.path.exists(installer):
+        os.unlink(installer)
+
+    shutil.move(os.path.join(dist_path, 'nsis', installer), root_path)
+
+    print('signing NSIS installer')
+    system('signtool.exe sign /v /tr http://rfc3161timestamp.globalsign.com/advanced /td sha256 /n "Tinkerforge GmbH" ' + installer)
+
+    print('verifying signature')
+    system('signtool.exe verify /v /pa ' + installer)
+
+
+def build_linux_pkg():
+    print('building demo Debian package')
+    root_path = os.getcwd()
+
+    print('removing old build directories')
+    dist_path = os.path.join(root_path, 'dist')
+    egg_info_path = os.path.join(root_path, '{0}.egg-info'.format(UNDERSCORE_NAME))
+
+    if os.path.exists(dist_path):
+        shutil.rmtree(dist_path)
+
+    if os.path.exists(egg_info_path):
+        shutil.rmtree(egg_info_path)
+
+    print('calling setup.py sdist')
+    system('python setup.py sdist')
+
+    if os.path.exists(egg_info_path):
+        shutil.rmtree(egg_info_path)
+
+    print('copying build data')
+    build_data_path = os.path.join(root_path, 'build_data', 'linux', UNDERSCORE_NAME)
+    linux_path = os.path.join(dist_path, 'linux')
+    shutil.copytree(build_data_path, linux_path)
+
+    print('unpacking sdist tar file')
+    system('tar -x -C {0} -f {0}/{1}-{2}.tar.gz {1}-{2}/{1}'.format(dist_path, UNDERSCORE_NAME, DEMO_VERSION))
+
+    print('copying unpacked demo source')
+    unpacked_path = os.path.join(dist_path, '{0}-{1}'.format(UNDERSCORE_NAME, DEMO_VERSION), UNDERSCORE_NAME)
+    linux_share_path = os.path.join(linux_path, 'usr', 'share', UNDERSCORE_NAME)
+    shutil.copytree(unpacked_path, linux_share_path)
+
+    print('creating DEBIAN/control from template')
+    installed_size = int(check_output(['du', '-s', '--exclude', 'dist/linux/DEBIAN', 'dist/linux']).split('\t')[0])
+    control_path = os.path.join(linux_path, 'DEBIAN', 'control')
+    specialize_template(control_path, control_path,
+                        {'<<VERSION>>': DEMO_VERSION,
+                         '<<INSTALLED_SIZE>>': str(installed_size)})
+
+    print('changing directory modes to 0755')
+    system('find dist/linux -type d -exec chmod 0755 {} \;')
+
+    print('changing file modes')
+    system('find dist/linux -type f -perm 664 -exec chmod 0644 {} \;')
+    system('find dist/linux -type f -perm 775 -exec chmod 0755 {} \;')
+
+    print('changing owner to root')
+    system('sudo chown -R root:root dist/linux')
+
+    print('building Debian package')
+    system('dpkg -b dist/linux {0}-{1}_all.deb'.format(UNDERSCORE_NAME.replace('_', '-'), DEMO_VERSION))
+
+    print('changing owner back to original user')
+    system('sudo chown -R ${USER}:${USER} dist/linux')
+
+    if os.path.exists('/usr/bin/lintian'):
+        print('checking Debian package')
+        system('lintian --pedantic {0}-{1}_all.deb'.format(UNDERSCORE_NAME.replace('_', '-'), DEMO_VERSION))
+    else:
+        print('skipping lintian check')
+
+
+# run 'python build_pkg.py' to build the windows/linux/macos package
+if __name__ == '__main__':
+    if sys.platform != 'win32' and os.geteuid() == 0:
+        print('error: must not be started as root, exiting')
+        sys.exit(1)
+
+    if sys.platform.startswith('linux'):
+        build_linux_pkg()
+    elif sys.platform == 'win32':
+        build_windows_pkg()
+    elif sys.platform == 'darwin':
+        build_macos_pkg()
+    else:
+        print('error: unsupported platform: ' + sys.platform)
+        sys.exit(1)
+
+    print('done')
---
+++ demo/setup.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+"""
+Tabletop Weather Station Demo
+Copyright (C) 2019 Matthias Bolte <matthias@tinkerforge.com>
+
+setup.py: Setuptools script for Tabletop Weather Station Demo
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public
+License along with this program; if not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+"""
+
+import os
+import sys
+import glob
+from setuptools import setup, find_packages
+from tabletop_weather_station_demo.config import DEMO_VERSION
+
+UNDERSCORE_NAME = 'tabletop_weather_station_demo'
+CAMEL_CASE_NAME = 'Tabletop Weather Station Demo'
+DESCRIPTION = 'Demo for Tabletop Weather Station'
+
+# Find demo packages
+if sys.platform == 'darwin':
+    packages = find_packages() # FIXME: setuptools on our macbook doesn't understand 'include'
+else:
+    packages = find_packages(include=[UNDERSCORE_NAME, '{0}.*'.format(UNDERSCORE_NAME)])
+
+# Collect non-frozen package_data
+package_data = {}
+
+if sys.platform.startswith('linux'):
+    image_patterns = ['*.bmp', '*.png', '*.jpg']
+
+    for package in packages:
+        package_path = os.path.join(*package.split('.'))
+        images = []
+
+        for pattern in image_patterns:
+            images += glob.glob(os.path.join(package_path, pattern))
+
+        if len(images) > 0:
+            package_data[package] = [os.path.basename(d) for d in images]
+
+    package_data[UNDERSCORE_NAME].append('{0}.desktop'.format(UNDERSCORE_NAME))
+
+# Collect platform specific data_files
+def collect_data_files(path, excludes=None):
+    path = os.path.normcase(path)
+    files = []
+
+    for root, dirnames, names in os.walk(path):
+        for name in names:
+            if excludes != None and name in excludes:
+                continue
+
+            full_name = os.path.join(root, name)
+
+            if os.path.isfile(full_name):
+                files.append((os.path.join(root.replace(path, '')), [full_name]))
+
+    return files
+
+data_files = []
+
+if sys.platform.startswith('linux'):
+    data_files.append(('/usr/share/pixmaps/', ['{0}/{0}-icon.png'.format(UNDERSCORE_NAME)]))
+    data_files.append(('/usr/share/applications/', ['{0}/{0}.desktop'.format(UNDERSCORE_NAME)]))
+elif sys.platform == 'win32':
+    data_files += collect_data_files('build_data/windows/', ['{0}-icon.ico'.format(UNDERSCORE_NAME)])
+elif sys.platform == 'darwin':
+    data_files += collect_data_files('build_data/macos/')
+
+# Run setup
+setup_arguments = {
+    'name':         UNDERSCORE_NAME,
+    'version':      DEMO_VERSION,
+    'author':       'Tinkerforge',
+    'author_email': 'info@tinkerforge.com',
+    'url':          'http://www.tinkerforge.com',
+    'license':      'GPL v2',
+    'description':  DESCRIPTION,
+    'packages':     packages,
+    'package_data': package_data,
+    'data_files':   data_files
+}
+
+if sys.platform.startswith('linux'):
+    setup_arguments['scripts'] = ['{0}/{0}'.format(UNDERSCORE_NAME)]
+elif sys.platform == 'win32':
+    import py2exe
+
+    options = {
+        'py2exe' : {
+            'dll_excludes': ['MSVCP90.dll'],
+            'includes':     ['sip',
+                             'PyQt4.QtCore',
+                             'PyQt4.QtGui',
+                             'win32process'],
+            'excludes':     ['PyQt4.QtOpenGL',
+                             'PyQt4.QtSvg',
+                             'PyQt4.Qwt5',
+                             'OpenGL.GL',
+                             'config_linux',
+                             'config_macos',
+                             '_gtkagg',
+                             '_tkagg',
+                             'Tkconstants',
+                             'Tkinter',
+                             'tcl',
+                             'pydoc',
+                             'email',
+                             'nose',
+                             'pdb',
+                             'inspect',
+                             'doctest',
+                             'difflib',
+                             'numpy.numarray',
+                             'numpy.oldnumeric',
+                             'numpy.core._dotblas',
+                             'numpy.random',
+                             'ctypes.macholib',
+                             'win32pdh',
+                             'win32ui']
+        }
+    }
+
+    windows = [{
+        'script':        '{0}/main.py'.format(UNDERSCORE_NAME),
+        'dest_base':     UNDERSCORE_NAME,
+        'icon_resources': [(0, os.path.normcase('build_data/windows/{0}-icon.ico'.format(UNDERSCORE_NAME)))]
+    }]
+
+    setup_arguments['options'] = options
+    setup_arguments['windows'] = windows
+    setup_arguments['zipfile'] = None
+elif sys.platform == 'darwin':
+    options = {
+        'py2app': {
+            'argv_emulation': True,
+            'iconfile':       'build_data/macos/{0}-icon.icns'.format(UNDERSCORE_NAME),
+            'site_packages':  True,
+            'includes':       ['atexit',
+                               'sip',
+                               'PyQt4.QtCore',
+                               'PyQt4.QtGui'],
+            'excludes':       ['scipy',
+                               'numpy',
+                               'distutils',
+                               'setuptools',
+                               'email',
+                               'matplotlib',
+                               'OpenGL',
+                               'PyQt4.QtOpenGL',
+                               'PyQt4.QtSvg',
+                               'PyQt4.Qwt5',
+                               'PyQt4.QtDeclarative',
+                               'PyQt4.QtDesigner',
+                               'PyQt4.QtHelp',
+                               'PyQt4.QtMultimedia',
+                               'PyQt4.QtNetwork',
+                               'PyQt4.QtScript',
+                               'PyQt4.QtScriptTools',
+                               'PyQt4.QtSql',
+                               'PyQt4.QtTest',
+                               'PyQt4.QtWebKit',
+                               'PyQt4.QtXml',
+                               'PyQt4.QtXmlPatterns']
+        }
+    }
+
+    app = [{
+        'script': '{0}/main.py'.format(UNDERSCORE_NAME),
+        'plist': {
+            'CFBundleName':               CAMEL_CASE_NAME,
+            'CFBundleVersion':            DEMO_VERSION,
+            'CFBundleShortVersionString': DEMO_VERSION,
+            'CFBundleGetInfoString':      DESCRIPTION,
+            'CFBundleExecutable':         UNDERSCORE_NAME,
+            'CFBundleIdentifier':         'com.tinkerforge.{0}'.format(UNDERSCORE_NAME),
+            'CFBundleIconFile':           '{0}-icon.icns'.format(UNDERSCORE_NAME),
+            'NSHumanReadableCopyright':   'Tinkerforge GmbH 2013-2015'
+        }
+    }]
+
+    setup_arguments['options']   = options
+    setup_arguments['scripts']   = ['{0}/main.py'.format(UNDERSCORE_NAME)]
+    setup_arguments['app']       = app
+
+setup(**setup_arguments)
